9aefb80c74ecfd4a73ab494eff8353fcab02ae97
Baryshnikov Nikolay <root@k0d.ru>
2019-08-16 02:20:53 +0300

    Обработка ошибок

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Интерфейс для перевода через Yandex Translate API
 
+Получить ключ тут: https://translate.yandex.ru/developers/keys
+
 Пример:
 
     In [1]: from main import Translate as T
     
-    In [2]: t = T('hello world')
+    In [2]: t = T('hello world', key='trnsl.1.1.20190815T224810Z.6a200a0ae36976a0.add72ad00ac7bd0463d227d57446b55a96a82535')
     
-    In [3]: t.translate()
-    Out[3]: {'code': 200, 'lang': 'en-ru', 'text': ['Привет мир']}
+    In [3]: t.get_price()
+    Out[3]: '0.010725'
+    
+    In [4]: t.translate()
+    Out[4]: {'code': 200, 'lang': 'en-ru', 'text': ['Привет мир']}
--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import os
 
 import requests
 
+class APIKeyException(BaseException):
+    """Не указан API ключ"""
+
 class Translate:
     URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-    KEY = ''
     PRICE = lambda content: (len(content) / 1000000)*15
+    KEY = ''
 
-    def __init__(self, content):
+    def __init__(self, content, key=None):
+        self.key = key or Translate.KEY
         self.source = content
 
     def get_price(self):
         return f'{Translate.PRICE(self.source)*65}'
 
     def translate(self):
+        if not self.key:
+            raise APIKeyException
+
         response = requests.get(
             self.URL,
             params = dict(
-                key=self.KEY,
+                key=self.key,
                 text=self.source,
                 lang='en-ru',
                 format='plain'
